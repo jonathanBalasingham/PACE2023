19e1e6b427f810b2c8474cb411d0f38be5ba17dc
jonathanBalasingham <jonbalasin@gmail.com>
2023-06-01 17:11:40 +0100

    add build dir

---
+++ build/CMakeFiles/PACE.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jonathan/CLionProjects/PACE/dm.c" "CMakeFiles/PACE.dir/dm.c.o" "gcc" "CMakeFiles/PACE.dir/dm.c.o.d"
+  "/home/jonathan/CLionProjects/PACE/roaring.c" "CMakeFiles/PACE.dir/roaring.c.o" "gcc" "CMakeFiles/PACE.dir/roaring.c.o.d"
+  "/home/jonathan/CLionProjects/PACE/main.cpp" "CMakeFiles/PACE.dir/main.cpp.o" "gcc" "CMakeFiles/PACE.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/PACE.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PACE.dir/dm.c.o"
+  "CMakeFiles/PACE.dir/dm.c.o.d"
+  "CMakeFiles/PACE.dir/main.cpp.o"
+  "CMakeFiles/PACE.dir/main.cpp.o.d"
+  "CMakeFiles/PACE.dir/roaring.c.o"
+  "CMakeFiles/PACE.dir/roaring.c.o.d"
+  "PACE"
+  "PACE.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/PACE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
